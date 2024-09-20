75a0fc57a82f92690a1e74e8238b431f4f1ac666
LeonCicak <leoncicak07@gmail.com>
2024-09-20 17:24:11 +0200

    Add files via upload

---
+++ mojaDrugaVjezba/mojaDrugaVjezba/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mojaDrugaVjezba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            int x, y, z, a;
+        Console.Write("upisi prvi broj:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("upisi drugi broj:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("upisi treci broj:");
+            z = Convert.ToInt32(Console.ReadLine());
+            a = x + y + z;
+            Console.WriteLine(x + " plus " + y +
+                " plus " + z + " jednako je " + a);
+            Console.ReadKey();
+        {
+        }
+    }
+}
